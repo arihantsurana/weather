3c2f95e227bca3e1b7f9029c747d4f7b06908ebf
Arihant Surana <arihant.surana@bigcommerce.com>
2017-12-11 13:48:25 +1100

    revert to master

--- exec.sh
+++ exec.sh
@@ -6,7 +6,6 @@ docker run \
   -v $PWD/data:/data \
   -p 5000-5010:5000-5010 \
   -e SCM_URL="https://github.com/arihantsurana/weather.git" \
-  -e SCM_BRANCH="setup-weather-java" \
   -e SPARK_DRIVER_HOST="172.25.8.30" \
   -e MAIN_CLASS="com.arihantsurana.weather.WeatherGenerator" \
   -e APP_ARGS="" \
