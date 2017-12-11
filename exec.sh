2453382d4a97ead2ccd5782d8c4d6ff146734707
Arihant Surana <arihant.surana@bigcommerce.com>
2017-12-11 12:34:20 +1100

    Add sample Spark app to test docker reading data

--- exec.sh
+++ exec.sh
@@ -3,9 +3,11 @@
 docker run \
   -ti \
   --rm \
+  -v /data:./data \
   -p 5000-5010:5000-5010 \
   -e SCM_URL="https://github.com/arihantsurana/weather.git" \
   -e SCM_BRANCH="setup-weather-java" \
   -e SPARK_DRIVER_HOST="172.25.8.30" \
   -e MAIN_CLASS="com.arihantsurana.weather.WeatherGenerator" \
+  -e APP_ARGS="" \
   tashoyan/docker-spark-submit:spark-2.2.0
--- src/main/scala/com/arihantsurana/weather/WeatherGenerator.scala
+++ src/main/scala/com/arihantsurana/weather/WeatherGenerator.scala
@@ -1,8 +1,39 @@
 package com.arihantsurana.weather
 
+import org.apache.log4j.LogManager
+import org.apache.spark.sql.SparkSession
+
 /**
   * Created by arihant.surana on 11/12/17.
   */
-class WeatherGenerator {
 
+/**
+  * Created by arihant.surana on 15/09/2016.
+  *
+  * primary Streaming object to handle Jirafe events and an entrypoint to Spark Streaming job
+  */
+object WeatherGenerator {
+
+  def main(args: Array[String]) {
+    val log = LogManager.getRootLogger
+    args.foreach(s => log.info(s"Argument - ${s}"))
+
+    val spark = SparkSession
+      .builder()
+      .appName("WeatherGenerator")
+      .getOrCreate()
+    val inputPath =
+      s"./data/somefile.txt"
+    val outputPath =
+      s"./data/output.txt"
+    log.info(s"Output path set to ${outputPath}")
+
+    val sc = spark.sparkContext
+    val textFile = sc.textFile(inputPath)
+    val counts = textFile.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+    counts.saveAsTextFile(outputPath)
+    spark.stop()
+  }
 }
